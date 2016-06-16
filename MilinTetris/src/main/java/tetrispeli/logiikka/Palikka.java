611f3a4dbadbc0930fbe6d049f41e05077f901e1
Annette Mili Kemppi <annette.kemppi@helsinki.fi>
2016-06-16 17:04:26 +0300

    Update Palikka.java

--- MilinTetris/src/main/java/tetrispeli/logiikka/Palikka.java
+++ MilinTetris/src/main/java/tetrispeli/logiikka/Palikka.java
@@ -18,15 +18,11 @@ import java.util.Random;
 public abstract class Palikka {
 
     private ArrayList<Osa> osat;
-    public boolean[][] ruudukko;
     private int x;
     private int y;
-    private Osa osa;
-    
 
     public Palikka(int x, int y) {
-        this.osat = osat;
-        this.ruudukko = ruudukko;
+        this.osat = new ArrayList<>();
         this.x = x;
         this.y = y;
     }
@@ -43,98 +39,34 @@ public abstract class Palikka {
         return osat;
     }
     
-    public void siirra(int x, int y) {
-        for (Osa o : osat) {
-            o.siirra(x, y);
-        }
+    public void siirraAlas() {
+        this.y += 1;
     }
     
-    public void setRuudukko(boolean[][] kaannos) {
-        
-    }
-
-    public boolean[][] getRuudukko() {
-        return ruudukko;
-    }
-    // globaalit ja lokaalit paras tapa!
-    // matriisikertolasku-taulukon teko
-    
-    public boolean meneekoPaalle(ArrayList<Osa> osat, Osa osa) {
-        for (Osa o : osat) {
-            if (o.getX() == osa.getX() && o.getY() == osa.getY()) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean onkoTyhja() {
-        int rivi = 0;
-        while (rivi < ruudukko.length) {
-            rivi++;
-            for (int sarake = 0; sarake < ruudukko.length; sarake++) {
-                if (ruudukko[rivi][sarake]) {
-                    return false;
-                }
-            }
-        }
-        return true;
+    public void siirraYlos() {
+        this.y -= 1;
     }
     
-    public boolean[][] kierraOikealle() {
-        boolean[][] uusi = new boolean[ruudukko.length][ruudukko.length];
-        int i = 0;
-        int j = ruudukko.length - 1;
-        int a = 0;
-        int b = 0;
-        while (a < ruudukko.length) {
-            a++;
-            i++;
-            while (b < ruudukko.length) {
-                j--;
-                b++;
-                uusi[a][b] = ruudukko[j][i];
-            }
+    public void siirraVasemmalle() {
+        this.x -= 1;
+    }
+    
+    public void siirraOikealle() {
+        this.x += 1;
+    }
+    
+    public void kierraOikealle() {
+        for (Osa osa : osat) {
+            int vanhaX = osa.getX();
+            int vanhaY = osa.getY();
+            osa.setX(vanhaY);
+            osa.setY(-vanhaX);
         }
-        return uusi;
     }
     
     public void kierraVasemmalle(int leveys, int korkeus) {   
-        kierraKerran();
-        if (meneekoSeinastaLapi(Suunta.VASEN, leveys, korkeus)
-                || meneekoSeinastaLapi(Suunta.OIKEA, leveys, korkeus)
-                || meneekoSeinastaLapi(Suunta.ALAS, leveys, korkeus)) {
-            for (int i = 1; i <= 3; i++) {
-                kierraKerran();
-            }
-        } 
-    }
-    
-    public void kaanna() {
-        for (int i = 1; i < osat.size(); i++) {
-            Osa o = (Osa) osat.get(i);
-            osat.get(i - 1);
-            o.kaannaVasemmalle();
-        }
-    }
-    
-    public boolean meneekoSeinastaLapi(Suunta suunta, int leveys, int korkeus) {
-        if (suunta == Suunta.ALAS) {
-            return (this.y > (korkeus - 2));
-        } else if (suunta == Suunta.OIKEA) {
-            return (this.x > (leveys - 1));
-        } else if (suunta == Suunta.VASEN) {
-            return (this.x < 0);
-        } else {
-            return (this.y < 0);
-        }
-    }
-    
-    public void kierraKerran() {
-        for (int i = 1; i < osat.size(); i++) {
-            Osa osa = (Osa) osat.get(i);
-            osat.get(i - 1);
-            osa.kaannaVasemmalle();
+        for (int i = 0; i < 3; i++) {
+            kierraOikealle();
         }
     }
 }
