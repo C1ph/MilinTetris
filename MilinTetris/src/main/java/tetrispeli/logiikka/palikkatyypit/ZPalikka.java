a48ec5419802774bfdae81bb82284cb481fded24
Annette Mili Kemppi <annette.kemppi@helsinki.fi>
2016-06-16 17:12:33 +0300

    Update ZPalikka.java

--- MilinTetris/src/main/java/tetrispeli/logiikka/palikkatyypit/ZPalikka.java
+++ MilinTetris/src/main/java/tetrispeli/logiikka/palikkatyypit/ZPalikka.java
@@ -18,11 +18,9 @@ public class ZPalikka extends Palikka {
 
     public ZPalikka(int x, int y) {
         super(x, y);
-        boolean[][] uusiRuudukko = {
-            {false, true, false},
-            {true, true, true},
-            {false, false, false}
-        };
-        super.ruudukko = uusiRuudukko;
+        super.getOsat().add(new Osa(0,0));
+        super.getOsat().add(new Osa(1,0));
+        super.getOsat().add(new Osa(0,1));
+        super.getOsat().add(new Osa(-1,1));
     }
 }
